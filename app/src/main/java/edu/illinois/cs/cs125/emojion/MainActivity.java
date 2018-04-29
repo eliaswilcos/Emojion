3de3f08acd1b8d3fff8639ef04b1e7493b2a0d06
eliaswilcos1 <elias.wilcoski@gmail.com>
2018-04-29 16:53:02 -0500

    Finished Emtoioncheck

--- app/src/main/java/edu/illinois/cs/cs125/emojion/MainActivity.java
+++ app/src/main/java/edu/illinois/cs/cs125/emojion/MainActivity.java
@@ -10,6 +10,9 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 import android.app.*;
 import android.content.*;
 import android.net.*;
@@ -19,6 +22,7 @@ import android.graphics.*;
 import android.widget.*;
 import android.provider.*;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.microsoft.projectoxford.face.*;
@@ -93,11 +97,98 @@ public class MainActivity extends AppCompatActivity {
         return bitmap;
     }
 
-    private static void checkEmotion(Face[] faces) {
+    public static Bitmap getBitmapFromURL(String src) {
+        try {
+            URL url = new URL(src);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoInput(true);
+            connection.connect();
+            InputStream input = connection.getInputStream();
+            Bitmap myBitmap = BitmapFactory.decodeStream(input);
+            return myBitmap;
+        } catch (IOException e) {
+            // Log exception
+            return null;
+        }
+    }
+
+    private void checkEmotion(Face[] faces) {
         String face = faces[0].toString();
         JsonParser parser = new JsonParser();
         JsonObject result = parser.parse(face).getAsJsonObject();
-        if (result == null) {
+        JsonArray attributes = result.getAsJsonArray("faceAttributes");
+        JsonArray emotions = attributes.get(0).getAsJsonArray();
+        JsonObject anger = emotions.get(0).getAsJsonObject();
+        double anger1 = anger.get("anger").getAsDouble();
+        JsonObject contempt = emotions.get(1).getAsJsonObject();
+        double contempt1 = contempt.get("contempt").getAsDouble();
+        JsonObject disgust = emotions.get(2).getAsJsonObject();
+        double disgust1 = disgust.get("disgust").getAsDouble();
+        JsonObject fear = emotions.get(3).getAsJsonObject();
+        double fear1 = fear.get("fear").getAsDouble();
+        JsonObject happiness = emotions.get(4).getAsJsonObject();
+        double happiness1 = happiness.get("happiness").getAsDouble();
+        JsonObject neutral = emotions.get(5).getAsJsonObject();
+        double neutral1 = neutral.get("neutral").getAsDouble();
+        JsonObject sadness = emotions.get(6).getAsJsonObject();
+        double sadness1 = sadness.get("sadness").getAsDouble();
+        JsonObject surprise = emotions.get(7).getAsJsonObject();
+        double surprise1 = surprise.get("surprise").getAsDouble();
+        double [] emotionArray = new double[]{anger1, contempt1, disgust1, fear1, happiness1, neutral1, sadness1, surprise1};
+        double largest = 0;
+        int emotion = 0;
+        for (int i = 0; i < 8; i++) {
+            if (emotionArray[i] > largest) {
+                largest = emotionArray[i];
+                emotion = i;
+            }
+        }
+        if (emotion == 0) {
+            ImageView imageView = (ImageView) findViewById(R.id.imageView1);
+            Bitmap bitmap = getBitmapFromURL("https://cdn.shopify.com/s/files/1/1061/1924/files/Very_Angry_Emoji.png?9898922749706957214");
+            imageView.setImageBitmap(bitmap);
+            return;
+        }
+        if (emotion == 1) {
+            ImageView imageView = (ImageView) findViewById(R.id.imageView1);
+            Bitmap bitmap = getBitmapFromURL("https://i.pinimg.com/originals/b0/5d/05/b05d05aae302296c24ccc845b36aedce.jpg");
+            imageView.setImageBitmap(bitmap);
+            return;
+        }
+        if (emotion == 2) {
+            ImageView imageView = (ImageView) findViewById(R.id.imageView1);
+            Bitmap bitmap = getBitmapFromURL("https://emojipedia-us.s3.amazonaws.com/thumbs/120/apple/129/nauseated-face_1f922.png");
+            imageView.setImageBitmap(bitmap);
+            return;
+        }
+        if (emotion == 3) {
+            ImageView imageView = (ImageView) findViewById(R.id.imageView1);
+            Bitmap bitmap = getBitmapFromURL("http://s3.amazonaws.com/pix.iemoji.com/images/emoji/apple/ios-11/256/face-screaming-in-fear.png");
+            imageView.setImageBitmap(bitmap);
+            return;
+        }
+        if (emotion == 4) {
+            ImageView imageView = (ImageView) findViewById(R.id.imageView1);
+            Bitmap bitmap = getBitmapFromURL("https://cdn.shopify.com/s/files/1/1061/1924/products/Happy_Emoji_Icon_5c9b7b25-b215-4457-922d-fef519a08b06_large.png?v=1513251037");
+            imageView.setImageBitmap(bitmap);
+            return;
+        }
+        if (emotion == 5) {
+            ImageView imageView = (ImageView) findViewById(R.id.imageView1);
+            Bitmap bitmap = getBitmapFromURL("https://cdn.shopify.com/s/files/1/1061/1924/files/Neutral_Face_Emoji.png?9898922749706957214");
+            imageView.setImageBitmap(bitmap);
+            return;
+        }
+        if (emotion == 6) {
+            ImageView imageView = (ImageView) findViewById(R.id.imageView1);
+            Bitmap bitmap = getBitmapFromURL("http://www.freepngimg.com/thumb/sad_emoji/36857-1-sad-emoji-thumb.png");
+            imageView.setImageBitmap(bitmap);
+            return;
+        }
+        if (emotion == 7) {
+            ImageView imageView = (ImageView) findViewById(R.id.imageView1);
+            Bitmap bitmap = getBitmapFromURL("https://cdn.shopify.com/s/files/1/1061/1924/products/Surprised_Face_Emoji_7113e110-82a7-493a-9bb1-7bdca77a661a_large.png?v=1480481057");
+            imageView.setImageBitmap(bitmap);
             return;
         }
         return;
